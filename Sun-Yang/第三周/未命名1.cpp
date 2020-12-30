23e5d42271a79ef7fdba578e542adc32dabd171c
sy12352 <1752298077@qq.com>
2020-12-30 10:56:13 +0800

    t

---
+++ Sun-Yang/第三周/未命名1.cpp
@@ -0,0 +1,34 @@
+
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	int a[100001] = {0};
+	int n;
+	cin >> n;
+	while(n != 0)
+	{
+		int j, k;
+		for(int i = 0; i < n; i ++)
+		{
+			
+			cin >> j >> k;
+			for(int ii = j;ii <= k; ii ++)
+			{
+				a[ii] ++;
+			}
+		}
+		for(int ii = 1;ii <= n;ii ++)
+		{
+			cout << a[ii];
+			if(ii!=k) cout <<" ";
+		}
+		cout << endl;
+		memset(a,0,sizeof(a));
+		cin >> n;
+	}
+	
+ } 
